cc6edf4a011064b6527aed5a2d89921521844adc
calvinhus <gjpcalvinho@gmail.com>
2022-01-19 18:58:06 +0000

    added keyboard exception

--- your-code/main.py
+++ your-code/main.py
@@ -1,15 +1,32 @@
 # define rooms and items
+import pygame
 from time import sleep
 import os
 
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+plt.close()
+relative_path = os.getcwd()
+game_sound = relative_path + '/your-code/sound/time.wav'
+end_sound = relative_path + '/your-code/sound/do_it_end.wav'
+ImageAddress = relative_path + "/escape-room-plan.jpg"
+ImageItself = Image.open(ImageAddress)
+ImageNumpyFormat = np.asarray(ImageItself)
+plt.imshow(ImageNumpyFormat)
+plt.draw()
+plt.pause(5)  # pause how many seconds
+# plt.close()
 # win sound effect path
 if os.name == 'posix':
     player = "afplay "
 else:
     player = "start "
 
-relative_path = os.getcwd()
-file = relative_path + '/your-code/sound/win.wav'
+
+pygame.mixer.init()
+pygame.mixer.music.load(game_sound)
+pygame.mixer.music.play()
 
 whiteboard = {
     "name": "whiteboard",
@@ -132,115 +149,114 @@ INIT_GAME_STATE = {
     "target_room": grad_party
 }
 
+try:
+    def linebreak():
+        """
+        Print a line break
+        """
+        print("\n\n")
 
-def linebreak():
-    """
-    Print a line break
-    """
-    print("\n\n")
+    def start_game():
+        """
+        Start the game
+        """
+        print("\nYou wake up on a whiteboard and find yourself in a strange house with no windows which you have never been to before.\nYou don't remember why you are here and what had happened before.\nYou feel some unknown danger is approaching and you must get out of the house, NOW!\n")
+        play_room(game_state["current_room"])
 
-
-def start_game():
-    """
-    Start the game
-    """
-    print("\nYou wake up on a whiteboard and find yourself in a strange house with no windows which you have never been to before.\nYou don't remember why you are here and what had happened before.\nYou feel some unknown danger is approaching and you must get out of the house, NOW!\n")
-    play_room(game_state["current_room"])
-
-
-def play_room(room):
-    """
-    Play a room. First check if the room being played is the target room.
-    If it is, the game will end with success. Otherwise, let player either 
-    explore (list all items in this room) or examine an item found here.
-    """
-    game_state["current_room"] = room
-    if(game_state["current_room"] == game_state["target_room"]):
-        print("\nCongrats! You escaped the room!")
-        os.system(player + file)
-        sleep(5)
-    else:
-        print("You are now in " + room["name"])
-        intended_action = input(
-            "\nWhat would you like to do? Type 'explore' or 'examine'?").strip()
-        if intended_action == "explore":
-            explore_room(room)
-            play_room(room)
-        elif intended_action == "examine":
-            examine_item(input("\nWhat would you like to examine?").strip())
+    def play_room(room):
+        """
+        Play a room. First check if the room being played is the target room.
+        If it is, the game will end with success. Otherwise, let player either 
+        explore (list all items in this room) or examine an item found here.
+        """
+        game_state["current_room"] = room
+        if(game_state["current_room"] == game_state["target_room"]):
+            print("\nCongrats! You escaped the room!")
+            os.system(player + end_sound)
+            sleep(5)
+            plt.close()
         else:
-            print("\nNot sure what you mean. Type 'explore' or 'examine'.")
-            play_room(room)
-        linebreak()
-
-
-def explore_room(room):
-    """
-    Explore a room. List all items belonging to this room.
-    """
-    items = [i["name"] for i in object_relations[room["name"]]]
-    print("\nYou explore it. This is " +
-          room["name"] + ". You find " + ", ".join(items))
-
-
-def get_next_room_of_door(door, current_room):
-    """
-    From object_relations, find the two rooms connected to the given door.
-    Return the room that is not the current_room.
-    """
-    connected_rooms = object_relations[door["name"]]
-    for room in connected_rooms:
-        if(not current_room == room):
-            return room
-
-
-def examine_item(item_name):
-    """
-    Examine an item which can be a door or furniture.
-    First make sure the intended item belongs to the current room.
-    Then check if the item is a door. Tell player if key hasn't been 
-    collected yet. Otherwise ask player if they want to go to the next
-    room. If the item is not a door, then check if it contains keys.
-    Collect the key if found and update the game state. At the end,
-    play either the current or the next room depending on the game state
-    to keep playing.
-    """
-    current_room = game_state["current_room"]
-    next_room = ""
-    output = None
-
-    for item in object_relations[current_room["name"]]:
-        if(item["name"] == item_name):
-            output = "\nYou examine " + item_name + ". "
-            if(item["type"] == "door"):
-                have_key = False
-                for key in game_state["keys_collected"]:
-                    if(key["target"] == item):
-                        have_key = True
-                if(have_key):
-                    output += "\nYou delivered the project!"
-                    next_room = get_next_room_of_door(item, current_room)
-                else:
-                    output += "\nIt is locked but you don't have the key."
+            print("You are now in " + room["name"])
+            intended_action = input(
+                "\nWhat would you like to do? Type 'explore' or 'examine'?").strip()
+            if intended_action == "explore":
+                explore_room(room)
+                play_room(room)
+            elif intended_action == "examine":
+                examine_item(
+                    input("\nWhat would you like to examine?").strip())
             else:
-                if(item["name"] in object_relations and len(object_relations[item["name"]]) > 0):
-                    item_found = object_relations[item["name"]].pop()
-                    game_state["keys_collected"].append(item_found)
-                    output += "\nYou find " + item_found["name"] + "."
+                print("\nNot sure what you mean. Type 'explore' or 'examine'.")
+                play_room(room)
+            linebreak()
+
+    def explore_room(room):
+        """
+        Explore a room. List all items belonging to this room.
+        """
+        items = [i["name"] for i in object_relations[room["name"]]]
+        print("\nYou explore it. This is " +
+              room["name"] + ". You find " + ", ".join(items))
+
+    def get_next_room_of_door(door, current_room):
+        """
+        From object_relations, find the two rooms connected to the given door.
+        Return the room that is not the current_room.
+        """
+        connected_rooms = object_relations[door["name"]]
+        for room in connected_rooms:
+            if(not current_room == room):
+                return room
+
+    def examine_item(item_name):
+        """
+        Examine an item which can be a door or furniture.
+        First make sure the intended item belongs to the current room.
+        Then check if the item is a door. Tell player if key hasn't been 
+        collected yet. Otherwise ask player if they want to go to the next
+        room. If the item is not a door, then check if it contains keys.
+        Collect the key if found and update the game state. At the end,
+        play either the current or the next room depending on the game state
+        to keep playing.
+        """
+        current_room = game_state["current_room"]
+        next_room = ""
+        output = None
+
+        for item in object_relations[current_room["name"]]:
+            if(item["name"] == item_name):
+                output = "\nYou examine " + item_name + ". "
+                if(item["type"] == "door"):
+                    have_key = False
+                    for key in game_state["keys_collected"]:
+                        if(key["target"] == item):
+                            have_key = True
+                    if(have_key):
+                        output += "\nYou delivered the project!"
+                        next_room = get_next_room_of_door(item, current_room)
+                    else:
+                        output += "\nIt is locked but you don't have the key."
                 else:
-                    output += "\nThere isn't anything interesting about it."
-            print(output)
-            break
+                    if(item["name"] in object_relations and len(object_relations[item["name"]]) > 0):
+                        item_found = object_relations[item["name"]].pop()
+                        game_state["keys_collected"].append(item_found)
+                        output += "\nYou find " + item_found["name"] + "."
+                    else:
+                        output += "\nThere isn't anything interesting about it."
+                print(output)
+                break
 
-    if(output is None):
-        print("\nThe item you requested is not found in the current room.")
+        if(output is None):
+            print("\nThe item you requested is not found in the current room.")
 
-    if(next_room and input("\nDo you want to go to the next module? Enter 'yes' or 'no': ").strip() == 'yes'):
-        play_room(next_room)
-    else:
-        play_room(current_room)
+        if(next_room and input("\nDo you want to go to the next module? Enter 'yes' or 'no': ").strip() == 'yes'):
+            play_room(next_room)
+        else:
+            play_room(current_room)
 
+    game_state = INIT_GAME_STATE.copy()
 
-game_state = INIT_GAME_STATE.copy()
-
-start_game()
+    start_game()
+except KeyboardInterrupt:
+    plt.close()
+    print("You interrupted the program execution.")
